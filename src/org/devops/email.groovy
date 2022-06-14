6f4b0b87d2276943f4809f0bc036bdcb51c99764
luvisi92 <45591348+luvisi92@users.noreply.github.com>
2022-06-14 17:05:00 +0800

    Update email.groovy

--- src/org/devops/email.groovy
+++ src/org/devops/email.groovy
@@ -2,8 +2,7 @@ package org.devops
 
 def Email(){
     emailext body: ''' <p>项目名称：${JOB_NAME}</p>         
-                       <p>构建编号：${BUILD_ID}</p> 
-                       <p>构建状态: ${status} </p>                         
+                       <p>构建编号：${BUILD_ID}</p>                        
                        <p>项目地址：<a href="${BUILD_URL}">${BUILD_URL}</a></p>    
                        <p>构建日志：<a href="${BUILD_URL}console">${BUILD_URL}console</a></p>''',subject: '构建结束', to: 'huangjuanzi@zhisoutech.com'  
 }
