90be5d49cbf13351d9ccde733bb7641d769a8cb2
luvisi92 <45591348+luvisi92@users.noreply.github.com>
2022-06-15 16:14:38 +0800

    Update toemail.groovy

--- src/org/devops/toemail.groovy
+++ src/org/devops/toemail.groovy
@@ -1,6 +1,6 @@
 package org.devops
 
-def Email(user){
+def Email(status,user){
     mail to: "huangjuanzi@zhisoutech.com",
     subject: "流水线邮件",
     body: """ <!DOCTYPE html> 
@@ -20,7 +20,7 @@ def Email(user){
                             <ul> 
                                 <li>项目名称：${JOB_NAME}</li>         
                                 <li>构建编号：${BUILD_ID}</li>
-                      
+                                <li>构建状态: ${status} </li> 
                                 <li>项目地址：<a href="${BUILD_URL}">${BUILD_URL}</a></li>    
                                 <li>构建日志：<a href="${BUILD_URL}console">${BUILD_URL}console</a></li> 
                             </ul> 
