f3146833b60d45911d85cb9ba479723923bd9082
luvisi92 <45591348+luvisi92@users.noreply.github.com>
2022-06-15 16:24:32 +0800

    Update toemail.groovy

--- src/org/devops/toemail.groovy
+++ src/org/devops/toemail.groovy
@@ -6,8 +6,8 @@ def Email(status,user){
     body: """ 
     构建信息：
         项目名称：${JOB_NAME}        
-        构建编号：${BUILD_ID}
+        <li>构建编号：${BUILD_ID}</li>
         构建状态: ${status}
-        项目地址：<a href="${BUILD_URL}">${BUILD_URL}</a>  
+       <li>项目地址：<a href="${BUILD_URL}">${BUILD_URL}</a></li>  
         构建日志：<a href="${BUILD_URL}console">${BUILD_URL}console</a> """
 }
