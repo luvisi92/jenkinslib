f71b021e9648c3d55cb77e831213dbb46fcbd8a7
luvisi92 <45591348+luvisi92@users.noreply.github.com>
2022-06-15 11:25:34 +0800

    Create toemail.groovy

---
+++ src/org/devops/toemail.groovy
@@ -0,0 +1,33 @@
+package org.devops
+
+def Email(status,user){
+    mail to: '${user}',
+    subject: '流水线邮件',
+    body: """ <!DOCTYPE html> 
+            <html> 
+            <head> 
+            <meta charset="UTF-8"> 
+            </head> 
+            <body> 
+                <table width="95%" cellpadding="0" cellspacing="0" style="font-size: 11pt; font-family: Tahoma, Arial, Helvetica, sans-serif">   
+                    <tr> 
+                        <td><br /> 
+                            <b><font color="#0B610B">构建信息</font></b> 
+                        </td> 
+                    </tr> 
+                    <tr> 
+                        <td> 
+                            <ul> 
+                                <li>项目名称：${JOB_NAME}</li>         
+                                <li>构建编号：${BUILD_ID}</li>
+                                <li>构建状态: ${status} </li>                         
+                                <li>项目地址：<a href="${BUILD_URL}">${BUILD_URL}</a></li>    
+                                <li>构建日志：<a href="${BUILD_URL}console">${BUILD_URL}console</a></li> 
+                            </ul> 
+                        </td> 
+                    </tr> 
+                    <tr>  
+                </table> 
+            </body> 
+            </html>"""
+}
